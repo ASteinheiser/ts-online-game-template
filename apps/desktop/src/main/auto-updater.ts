eadf5fa4dea1e88e06d957c86ba1782e21e60585
ASteinheiser <me@iamandrew.io>
2026-01-20 00:02:54 -0700

    revert to 0.0.1 for testing - comment out quit button - reduce update poll to 1 min

--- apps/desktop/src/main/auto-updater.ts
+++ apps/desktop/src/main/auto-updater.ts
@@ -3,7 +3,7 @@ import ElectronUpdater from 'electron-updater';
 import { ELECTRON_EVENTS } from '../shared/constants';
 
 const { autoUpdater } = ElectronUpdater;
-const POLL_INTERVAL = 10 * 60 * 1000; // 10 minutes
+const POLL_INTERVAL = 1 * 60 * 1000; // 1 minute
 
 /**
  * Initializes auto-updater to silently poll for and apply updates.
