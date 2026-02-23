89cde23b364fb276eb5a485a7cc59bf3c8f866db
ASteinheiser <me@iamandrew.io>
2026-02-22 20:05:57 -0700

    create DevLog #0 - The Vision

--- apps/web/src/pages/DevLog/entries/devlog-1.tsx
+++ apps/web/src/pages/DevLog/entries/devlog-1.tsx
@@ -1,19 +1,61 @@
+import { Link } from 'react-router-dom';
 import { DevLogEntry } from '../../../components/DevLogEntry';
 
 export const DevLogEntry1 = () => {
   return (
-    <DevLogEntry id={1} title="First Steps" date="April 24, 2025" author="Andrew Steinheiser">
+    <DevLogEntry id={0} title="The Vision" date="February 22, 2026" author="Andrew Steinheiser">
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
-        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat.
+        So initially, I was going to leave this DevLog section on the template blank (lorem ipsum
+        placeholder), but I decided I might as well write a little about what&apos;s going on with this
+        project.
       </p>
-      <img src="/logo.svg" alt="Logo" className="w-1/4 mx-auto" />
       <p>
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum.
+        My goal is to create a template that makes it easy to make real-time, online games using TypeScript!
+        I&apos;ll be using the template myself{' '}
+        <Link to="https://ore-rush.online" target="_blank" className="text-primary underline">
+          to make a game
+        </Link>
+        , and hope others might find it useful as well! I figure the more people using a similar setup will
+        lead to more information being shared, advancements made, etc.
       </p>
+      <p>
+        Here is a <i>rough</i> list of my motivations and goals for this template:
+      </p>
+      <ul className="list-disc pl-10 marker:content-['➤___']">
+        <li>
+          Create a desktop app (the game client), game server, and marketing site (with developer log,
+          download button and auth)
+        </li>
+        <li>
+          Do it all in a monorepo so you can easily share UI, game logic, or anything really across
+          &quot;apps&quot;
+        </li>
+        <li>
+          Create a more robust Phaser + Colyseus starter, which includes a &quot;Client Side Prediction and
+          Server Reconciliation&quot; demo. All game logic is run on the server, so clients simply send their
+          input (basic anti-cheat setup).
+        </li>
+        <li>
+          Clean slate to make whatever kind of game; which means you will need to BYOS (bring your own
+          systems), such as `miniplex` (ECS), etc. Make a classic mmorpg or maybe a card game! Whatever you
+          want!
+        </li>
+        <li>
+          Complete CI/CD flow that allows you to deploy and test your game live from day 1, with instructions
+          on how to set it all up
+        </li>
+        <li>Keep the hosting costs low, especially at the start</li>
+        <li>Test suites setup for each &quot;app&quot; and &quot;package&quot; in the monorepo</li>
+        <li>
+          Ensure fewer UI/visual bugs by leaning on Electron; all game clients will be running Chromium and
+          built for Windows, macOS and Linux
+        </li>
+        <li>
+          Ensure a consistent auth experience for users across the marketing site and desktop app (including
+          deep links). Currently, I use Supabase, but you could easily swap it out in the `client-auth`
+          package.
+        </li>
+      </ul>
     </DevLogEntry>
   );
 };
--- apps/web/src/pages/DevLog/index.tsx
+++ apps/web/src/pages/DevLog/index.tsx
@@ -1,14 +1,10 @@
 import { DevLogEntry1 } from './entries/devlog-1';
-import { DevLogEntry2 } from './entries/devlog-2';
-import { DevLogEntry3 } from './entries/devlog-3';
 
 export const DevLog = () => {
   return (
-    <div className="flex flex-col gap-10 max-w-screen-md mx-auto py-6 px-4">
+    <div className="flex flex-col gap-10 max-w-3xl mx-auto py-6 px-4">
       <h1 className="text-5xl sm:text-7xl text-center font-pixel text-primary">Developer Log</h1>
 
-      <DevLogEntry3 />
-      <DevLogEntry2 />
       <DevLogEntry1 />
     </div>
   );
